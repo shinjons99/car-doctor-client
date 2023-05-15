8a553ef73b7a50c0303d4dd45df7bba9e08d346c
shinjons99 <shinjons99@gmail.com>
2023-05-15 15:01:19 +0600

    First Commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <h2>This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
