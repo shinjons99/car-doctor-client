9d74052d0c6c0932a9b8c075827c8133a346ccb0
shinjons99 <shinjons99@gmail.com>
2023-05-16 08:28:16 +0600

    Style Banner Component with gradient overlay

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,31 +1,35 @@
 import React from "react";
-import img1 from '../../../assets/images/banner/1.jpg';
-import img2 from '../../../assets/images/banner/2.jpg';
-import img3 from '../../../assets/images/banner/3.jpg';
-import img4 from '../../../assets/images/banner/4.jpg';
-import img5 from '../../../assets/images/banner/5.jpg';
-import img6 from '../../../assets/images/banner/6.jpg';
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
 
 const Banner = () => {
 	return (
 		<div className='carousel w-full'>
 			<div id='slide1' className='carousel-item relative w-full h-[600px]'>
-				<img
-					src={img1}
-					className='w-full rounded-xl'
-				/>
-				<div className='absolute flex transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<div className="text-white">
-                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
-                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
-                        <div>
-                        <button className="btn btn-primary mr-5">Discover More</button> 
-                        <button className="btn btn-outline btn-secondary">Latest Project</button>
-                        </div>
-                    </div>
-                    </div>
+				<img src={img1} className='w-full rounded-xl' />
+				<div className='absolute h-full flex items-center left-0 top-0  bottom-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]'>
+					<div className='text-white space-y-7 pl-12 w-1/2'>
+						<h2 className='text-6xl font-bold'>
+							Affordable Price For Car Servicing
+						</h2>
+						<p>
+							There are many variations of passages of available, but the
+							majority have suffered alteration in some form
+						</p>
+						<div>
+							<button className='btn btn-primary mr-5'>Discover More</button>
+							<button className='btn btn-outline btn-secondary'>
+								Latest Project
+							</button>
+						</div>
+					</div>
+				</div>
 				<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<a href='#slide1' className='btn btn-circle mr-5'>
+					<a href='#slide4' className='btn btn-circle mr-5'>
 						❮
 					</a>
 					<a href='#slide2' className='btn btn-circle'>
@@ -33,13 +37,10 @@ const Banner = () => {
 					</a>
 				</div>
 			</div>
-			<div id='slide2' className='carousel-item relative w-full'>
-				<img
-					src={img2}
-					className='w-full'
-				/>
-					<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<a href='#slide2' className='btn btn-circle mr-5'>
+			<div id='slide2' className='carousel-item relative w-full h-[600px]'>
+				<img src={img2} className='w-full' />
+				<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
+					<a href='#slide1' className='btn btn-circle mr-5'>
 						❮
 					</a>
 					<a href='#slide3' className='btn btn-circle'>
@@ -47,13 +48,10 @@ const Banner = () => {
 					</a>
 				</div>
 			</div>
-			<div id='slide3' className='carousel-item relative w-full'>
-				<img
-					src={img3}
-					className='w-full'
-				/>
-					<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<a href='#slide3' className='btn btn-circle mr-5'>
+			<div id='slide3' className='carousel-item relative w-full h-[600px]'>
+				<img src={img3} className='w-full' />
+				<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
+					<a href='#slide2' className='btn btn-circle mr-5'>
 						❮
 					</a>
 					<a href='#slide4' className='btn btn-circle'>
@@ -61,13 +59,10 @@ const Banner = () => {
 					</a>
 				</div>
 			</div>
-			<div id='slide4' className='carousel-item relative w-full'>
-				<img
-					src={img4}
-					className='w-full'
-				/>
-					<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<a href='#slide4' className='btn btn-circle mr-5'>
+			<div id='slide4' className='carousel-item relative w-full h-[600px]'>
+				<img src={img4} className='w-full' />
+				<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
+					<a href='#slide3' className='btn btn-circle mr-5'>
 						❮
 					</a>
 					<a href='#slide5' className='btn btn-circle'>
@@ -75,13 +70,10 @@ const Banner = () => {
 					</a>
 				</div>
 			</div>
-			<div id='slide5' className='carousel-item relative w-full'>
-				<img
-					src={img5}
-					className='w-full'
-				/>
-					<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
-					<a href='#slide5' className='btn btn-circle mr-5'>
+			<div id='slide5' className='carousel-item relative w-full h-[600px]'>
+				<img src={img5} className='w-full' />
+				<div className='absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0'>
+					<a href='#slide4' className='btn btn-circle mr-5'>
 						❮
 					</a>
 					<a href='#slide1' className='btn btn-circle'>
